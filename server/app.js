a655e8c9fc099eb2582a93ce8d37018fd7552b9e
Jen <jenniferlynnebarry@gmail.com>
2022-04-29 15:23:58 -0700

    passing tests

--- server/app.js
+++ server/app.js
@@ -1,8 +1,47 @@
+const e = require('express');
 const express = require('express');
-
+var morgan = require('morgan');
+var movieData = require('../package.json');
+require('dotenv').config();
 
 const app = express();
+const api_key = process.env.API_KEY;
+const axios = require('axios').default;
+
+var cache = [];
+// add routes and middleware 
+app.use(morgan('dev'));
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+
+app.get('/', (req, res) => {
+
+    if (req.url[2] == "i") {
+        for (let i = 0; i < cache.length; i++) {
+            console.log(req.query["i"]);
+            if (req.url["i"] == cache[i]["imdbID"]) {
+                res.status(200).send(cache[i]);
+                return;
+        } 
+      }
+    }
+    if (req.url[2] == "t") {
+        console.log("inside else if");
+        for (let i = 0; i < cache.length; i++) {
+            if (req.query["t"] == cache[i]["Title"]) {
+                res.status(200).send(cache[i]);
+                return;
+            }
+        }
+    }
+    var url = `http://www.omdbapi.com${req.url}&apikey=${api_key}`;
+    axios.get(url)
+    .then(movieObj => {
+    cache.push(movieObj.data);
+    res.status(200).send(movieObj.data);
+    });
+
+});
+
 
 module.exports = app;
